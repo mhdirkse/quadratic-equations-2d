892bdf5c44e3a5397d1fb0a4382ee7d6b05496be
Martijn Dirkse <mhdirkse@live.nl>
2022-10-23 21:16:57 +0200

    Make code more beautiful

--- src/main.rs
+++ src/main.rs
@@ -12,77 +12,69 @@ mod get_primes {
     // remove all multiples of that prime from the list.
     // If a prime satisfies prime * prime > max_prime, there
     // will not be values to be removed from the list. If there
-    // were a value v <= max_prime that satisfies d1 * d2 == v,
-    // then d1 or d2 <= prime.
+    // were a value v <= max_prime that satisfies d * prime == v,
+    // d < prime. The value v would have been marked as non-prime
+    // when d or one of its factors would be considered for removing
+    // multitudes.
 
     use std::cmp::min;
 
     const TOP_ROOT_OF_U32: u32 = (1 as u32) << 16;
 
-    pub fn get_primes(max_prime: u32) -> Vec<u32> {
-        if max_prime <= 1 {
+    pub fn get_primes(prime_list_upper_bound: u32) -> Vec<u32> {
+        if prime_list_upper_bound <= 1 {
             return vec![];
         }
-        let mut result = vec![];
-        for i in 2 ..= max_prime {
-            result.push(i);
-        }
-        let max_current_prime = sqrt_floor(max_prime);
-        let mut current_prime_index = 0;
+        let mut prime_list: Vec<u32> = (2 ..= prime_list_upper_bound).collect();
+        let filter_upper_bound = sqrt_floor(prime_list_upper_bound);
+        let mut filter_index = 0;
         loop {
-            let current_prime = result[current_prime_index];
-            if current_prime > max_current_prime {
+            let filter = prime_list[filter_index];
+            if filter > filter_upper_bound {
                 break;
             }
-            result = remove_multiples_of(current_prime, result);
-            current_prime_index += 1;
+            prime_list.retain(|e| ! ((e % filter == 0) && (e / filter >= 2)));
+            filter_index += 1;
         }
-        return result;
+        return prime_list;
     }
 
+    // For a value v, get the maximum value r that satisfies r * r <= v.
+    // We maintain a value top that satisfies top * top > v.
+    // We also maintain a value min_root that satisfies min_root * min_root <= v.
+    // We adjust these bounds by bisection until min_root + 1 == top.
+    //
+    // We prove now that bisection always ends properly with min_root + 1 == top.
+    // If for some k we have min_root + k == top, then bisection produces
+    // middle := (2 * min_root + k) / 2 == min_root + k / 2.
+    // If k >= 2, we know that the distance between 0 and k/2 and
+    // the distance between k/2 and k will be strictly smaller than k and that
+    // neither of these distances will be zero. These are true because k / 2 >= 1.
+    // and because k - k/2 >= 1 for k >= 3.
     fn sqrt_floor(v: u32) -> u32 {
         if (v == 0) || (v == 1) {
             return v;
         }
-        return sqrt_bounds(1, min(v, TOP_ROOT_OF_U32), v);
-    }
-
-    fn sqrt_bounds(min_root: u32, top: u32, v: u32) -> u32 {
-        if top == (min_root + 1) {
-            return min_root;
-        }
-        let middle = get_middle(min_root, top);
-        if middle * middle <= v {
-            return sqrt_bounds(middle, top, v);
-        } else {
-            return sqrt_bounds(min_root, middle, v);
-        }
-    }
-
-    fn get_middle(v1: u32, v2: u32) -> u32 {
-        let v164 = v1 as u64;
-        let v264 = v2 as u64;
-        let middle64 = (v164 + v264) / 2;
-        return middle64 as u32;
-    }
-
-    fn remove_multiples_of(p: u32, v: Vec<u32>) -> Vec<u32> {
-        let mut result: Vec<u32> = vec![];
-        for e in v {
-            let quot = e / p;
-            let rem = e % p;
-            if ! ((rem == 0) && (quot >= 2)) {
-                result.push(e);
+        let mut min_root: u32 = 1;
+        let mut top: u32 = min(v, TOP_ROOT_OF_U32);
+        loop {
+            if top == (min_root + 1) {
+                return min_root;
+            }
+            let middle = (min_root + top) / 2;
+            if middle * middle <= v {
+                min_root = middle;
+            } else {
+                top = middle;
             }
         }
-        return result;
     }
 
     #[cfg(test)]
     mod tests {
         use super::{
+            get_primes,
             TOP_ROOT_OF_U32,
-            get_middle,
             sqrt_floor
         };
 
@@ -97,14 +89,6 @@ mod get_primes {
         fn max_root_of_uint32_is_2_pow_16() {
             let expected: u32 = (256 as u32) * (256 as u32);
             assert_eq!(expected, TOP_ROOT_OF_U32);
-        }
-
-        #[test]
-        fn middle_with_difference_2_does_not_repeat_bounds() {
-            assert_eq!(get_middle(3, 5), 4);
-            assert_eq!(get_middle(4, 6), 5);
-            assert_eq!(get_middle(5, 3), 4);
-            assert_eq!(get_middle(6, 4), 5);
         }
 
         #[test]
@@ -124,15 +108,15 @@ mod get_primes {
             assert_eq!(sqrt_floor(0xffff * 0xffff - 1), 0xfffe);
             assert_eq!(sqrt_floor(0xfffe * 0xfffe), 0xfffe);
         }
-    }
 
-    #[test]
-    fn test_primes() {
-        assert_eq!(get_primes(0), vec![]);
-        assert_eq!(get_primes(1), vec![]);
-        assert_eq!(get_primes(2), vec![2]);
-        assert_eq!(get_primes(9), vec![2, 3, 5, 7]);
-        assert_eq!(get_primes(10), vec![2, 3, 5, 7]);
-        assert_eq!(get_primes(25), vec![2, 3, 5, 7, 11, 13, 17, 19, 23]);
+        #[test]
+        fn test_primes() {
+            assert_eq!(get_primes(0), vec![]);
+            assert_eq!(get_primes(1), vec![]);
+            assert_eq!(get_primes(2), vec![2]);
+            assert_eq!(get_primes(9), vec![2, 3, 5, 7]);
+            assert_eq!(get_primes(10), vec![2, 3, 5, 7]);
+            assert_eq!(get_primes(25), vec![2, 3, 5, 7, 11, 13, 17, 19, 23]);
+        }
     }
 }
