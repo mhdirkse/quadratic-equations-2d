d2315eb0fbd8cfd1a0a8cb309d4c394705c27e9c
Martijn Dirkse <mhdirkse@live.nl>
2026-02-14 23:43:44 +0100

    Have multiplication of field values

--- src/field.rs
+++ src/field.rs
@@ -2,8 +2,9 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use std::fmt::Debug;
 use std::ops::Add;
+use std::ops::Mul;
 use num_rational::Rational32;
-use num_traits::CheckedAdd;
+use num_traits::{CheckedAdd, CheckedMul};
 pub type Base = Rational32;
 
 #[derive(Clone)]
@@ -93,56 +94,142 @@ impl RawFieldValue {
         }
     }
 
-    fn checked_add(&self, other: &Self) -> Option<Self> {
-        match self {
-            RawFieldValue::BASIC(base) => {
-                match other {
-                    RawFieldValue::BASIC(otherBase) => {
-                        let result = base.checked_add(otherBase)?;
-                        return Option::Some(RawFieldValue::BASIC(result));
-                    }
-                    RawFieldValue::EXTENDED(otherExtended) => {
-                        panic!("RawFieldValue::checked_add() extension depth mismatch: {} vs. {}",
-                            self.num_extensions(), other.num_extensions());
-                    }
+    fn checked_add(&self, other: &Self, context: &FieldTower) -> Option<Self> {
+        return bin_op(self, other, context, checked_add_base, checked_add_extended);
+    }
+
+    fn checked_mul(&self, other: &Self, context: &FieldTower) -> Option<Self> {
+        return bin_op(
+            self,
+            other,
+            context,
+            checked_mul_base,
+            checked_mul_extended);
+    }
+}
+
+fn bin_op(
+    raw_first: &RawFieldValue,
+    raw_second: &RawFieldValue,
+    context: &FieldTower,
+    base_op: fn(&Base, &Base, &FieldTower) -> Option<Base>,
+    extended_op: fn(&RawExtendedValue, &RawExtendedValue, &FieldTower) -> Option<RawExtendedValue>)
+    -> Option<RawFieldValue>
+{
+    let (first, second): (RawFieldValue, RawFieldValue) =
+        equalize_num_extensions(raw_first, raw_second);
+    match first {
+        RawFieldValue::BASIC(base) => {
+            match second {
+                RawFieldValue::BASIC(other_base) => {
+                    let result: Base = base_op(&base, &other_base, context)?;
+                    return Option::Some(RawFieldValue::BASIC(result));
+                }
+                RawFieldValue::EXTENDED(_) => {
+                    panic!("RawFieldValue::bin_op() extension depth mismatch: {} vs. {}",
+                        first.num_extensions(), second.num_extensions());
                 }
             }
-            RawFieldValue::EXTENDED(extended) => {
-                match other {
-                    RawFieldValue::BASIC(otherBase) => {
-                        panic!("RawFieldValue::checked_add() extension depth mismatch: {} vs. {}",
-                            self.num_extensions(), other.num_extensions());
-                    }
-                    RawFieldValue::EXTENDED(otherExtended) => {
-                        let resultBase: RawFieldValue = extended.base.checked_add(&otherExtended.base)?;
-                        let resultExtension: RawFieldValue = extended.extension.checked_add(&otherExtended.extension)?;
-                        return Option::Some(RawFieldValue::EXTENDED(RawExtendedValue {
-                             base: Box::new(resultBase),
-                             extension: Box::new(resultExtension)
-                        }));
-                    }
+        }
+        RawFieldValue::EXTENDED(ref extended) => {
+            match second {
+                RawFieldValue::BASIC(_) => {
+                    panic!("RawFieldValue::bin_op() extension depth mismatch: {} vs. {}",
+                        first.num_extensions(), second.num_extensions());
+                }
+                RawFieldValue::EXTENDED(ref other_extended) => {
+                    let result: RawExtendedValue = extended_op(extended, other_extended, context)?;
+                    return Option::Some(RawFieldValue::EXTENDED(result));
                 }
             }
         }
     }
 }
 
+fn checked_add_base(first: &Base, second: &Base, _: &FieldTower) -> Option<Base> {
+    return first.checked_add(second);
+}
+
+fn checked_add_extended(first: &RawExtendedValue, second: &RawExtendedValue, context: &FieldTower) -> Option<RawExtendedValue> {
+    let base: RawFieldValue = bin_op(
+        &first.base,
+        &second.base,
+        context,
+        checked_add_base,
+        checked_add_extended)?;
+    let extension: RawFieldValue = bin_op(
+        &first.extension,
+        &second.extension,
+        context,
+        checked_add_base,
+        checked_add_extended
+    )?;
+    return Option::Some(RawExtendedValue {
+        base: Box::new(base),
+        extension: Box::new(extension)
+    });
+}
+
+fn checked_mul_base(first: &Base, second: &Base, _: &FieldTower) -> Option<Base> {
+    return first.checked_mul(second);
+}
+
+fn checked_mul_extended(first: &RawExtendedValue, second: &RawExtendedValue, context: &FieldTower) -> Option<RawExtendedValue> {
+    let root_index: u32 = first.base.num_extensions();
+    let root: &RawFieldValue = &context.data.borrow().roots[root_index as usize];
+    let term1: RawFieldValue = bin_op(
+        &first.base,
+        &second.base,
+        context,
+        checked_mul_base,
+        checked_mul_extended
+    )?;
+    let term2: RawFieldValue = bin_op(
+        &first.base,
+        &second.extension,
+        context,
+        checked_mul_base,
+        checked_mul_extended
+    )?;
+    let term3: RawFieldValue = bin_op(
+        &first.extension,
+        &second.base,
+        context,
+        checked_mul_base,
+        checked_mul_extended
+    )?;
+    let term4: RawFieldValue = bin_op(
+        &first.extension,
+        &second.extension,
+        context,
+        checked_mul_base,
+        checked_mul_extended
+    )?;
+    let result_base: RawFieldValue = term1.checked_add(&term4.checked_mul(root, context)?, context)?;
+    let result_extension: RawFieldValue = term2.checked_add(&term3, context)?;
+    return Option::Some(RawExtendedValue {
+        base: Box::new(result_base),
+        extension: Box::new(result_extension)
+    });
+}
+
 impl PartialEq for RawFieldValue {
     fn eq(&self, other: &Self) -> bool {
-        let num_extensions_self = self.num_extensions();
-        let num_extensions_other = other.num_extensions();
-        if (num_extensions_self < num_extensions_other) {
-            return compare_raw_field_values_impl(
-                &promote_raw_field_value(self, num_extensions_other), 
-                other);
-        } else if (num_extensions_other < num_extensions_self) {
-            return compare_raw_field_values_impl(
-                &promote_raw_field_value(other, num_extensions_self),
-                self
-            );
-        } else {
-            return compare_raw_field_values_impl(self, other);
-        }
+        let (first, second): (RawFieldValue, RawFieldValue) =
+            equalize_num_extensions(&self, other);
+        return compare_raw_field_values_impl(&first, &second);
+    }
+}
+
+fn equalize_num_extensions(first: &RawFieldValue, second: &RawFieldValue) -> (RawFieldValue, RawFieldValue) {
+    let num_extensions_first = first.num_extensions();
+    let num_extensions_second = second.num_extensions();
+    if num_extensions_first < num_extensions_second {
+        return (promote_raw_field_value(&first, num_extensions_second), second.clone());
+    } else if num_extensions_second < num_extensions_first {
+        return (first.clone(), promote_raw_field_value(&second, num_extensions_first));
+    } else {
+        return (first.clone(), second.clone());
     }
 }
 
@@ -255,21 +342,39 @@ fn is_power_of_two(n: usize) -> bool {
 impl Add for FieldValue {
     type Output = Self;
 
-    fn add(self, other: Self) -> Self {
+    fn add(self, _: Self) -> Self {
         panic!("Not implemented");
     }
 }
 
 impl CheckedAdd for FieldValue {
-    // Not defined as implementing CheckedAdd. That would require
-    // implementing trait Add which is private.
     fn checked_add(&self, other: &Self) -> Option<Self> {
         if self.context != other.context {
             panic!("FieldValue::checked_add not allowed on value from different FieldTower instances")
         }
         return Option::Some(FieldValue {
             context: self.context.clone(),
-            value: self.value.checked_add(&other.value)?,
+            value: self.value.checked_add(&other.value, &self.context)?,
+        });
+    }
+}
+
+impl Mul for FieldValue {
+    type Output = Self;
+
+    fn mul(self, _: Self) -> Self {
+        panic!("Not implemented");
+    }
+}
+
+impl CheckedMul for FieldValue {
+    fn checked_mul(&self, other: &Self) -> Option<Self> {
+        if self.context != other.context {
+            panic!("FieldValue::checked_mul() not allowed on value from different FieldTower instances")
+        }
+        return Option::Some(FieldValue {
+            context: self.context.clone(),
+            value: self.value.checked_mul(&other.value, &self.context)?,
         });
     }
 }
@@ -278,7 +383,7 @@ impl CheckedAdd for FieldValue {
 mod test {
     use crate::field::{FieldTower, FieldValue, RawFieldValue};
     use num_rational::Rational32;
-    use num_traits::CheckedAdd;
+    use num_traits::{CheckedAdd, CheckedMul};
 
     #[test]
     fn field_tower_only_clones_are_equal() {
@@ -357,5 +462,55 @@ mod test {
         assert_eq!(with_coefficients_one.checked_add(&big_base).is_none(), true);
         assert_eq!(big_extended.checked_add(&with_coefficients_one).is_none(), true);
         assert_eq!(with_coefficients_one.checked_add(&big_extended).is_none(), true);
+    }
+
+    #[test]
+    fn values_with_simple_root_can_be_multiplied() {
+        let mut tower: FieldTower = FieldTower::new();
+        tower.add_root(tower.value(Rational32::new(2, 1)));
+        let coefficients_1: Vec<Rational32> = vec![Rational32::new(3, 1), Rational32::new(5, 1)];
+        let raw_test_value_1 = RawFieldValue::from_coefficients(&coefficients_1[..]);
+        let test_value_1 = FieldValue {
+            context: tower.clone(),
+            value: raw_test_value_1
+        };
+        let coefficients_2: Vec<Rational32> = vec![Rational32::new(10, 1), Rational32::new(20, 1)];
+        let raw_test_value_2 = RawFieldValue::from_coefficients(&coefficients_2[..]);
+        let test_value_2 = FieldValue {
+            context: tower.clone(),
+            value: raw_test_value_2
+        };
+        let optional_result: Option<FieldValue> = test_value_1.checked_mul(&test_value_2);
+        let result = optional_result.expect("Unexpected overflow");
+        let coefficients_expected: Vec<Rational32> = vec![Rational32::new(230, 1), Rational32::new(110, 1)];
+        let raw_expected_value = RawFieldValue::from_coefficients(&coefficients_expected[..]);
+        let expected_value = FieldValue {
+            context: tower,
+            value: raw_expected_value
+        };
+        assert_eq!(result.to_string(), expected_value.to_string());
+        assert_eq!(result == expected_value, true);
+    }
+
+    #[test]
+    fn when_multiplying_produces_overflow_than_none_returned() {
+        let mut tower: FieldTower = FieldTower::new();
+        tower.add_root(tower.value(Rational32::new(2, 1)));
+        let big_base = FieldValue {
+            context: tower.clone(),
+            value: RawFieldValue::from_coefficients(&vec![Rational32::new(i32::MAX, 1), Rational32::new(1, 1)])
+        };
+        let big_extended = FieldValue {
+            context: tower.clone(),
+            value: RawFieldValue::from_coefficients(&vec![Rational32::new(1, 1), Rational32::new(i32::MAX, 1)])
+        };
+        let multiplier = FieldValue {
+            context: tower.clone(),
+            value: RawFieldValue::from_coefficients(&vec![Rational32::new(2, 1), Rational32::new(2, 1)])
+        };
+        assert_eq!(big_base.checked_add(&multiplier).is_none(), true);
+        assert_eq!(multiplier.checked_add(&big_base).is_none(), true);
+        assert_eq!(big_extended.checked_add(&multiplier).is_none(), true);
+        assert_eq!(multiplier.checked_add(&big_extended).is_none(), true);
     }
 }
